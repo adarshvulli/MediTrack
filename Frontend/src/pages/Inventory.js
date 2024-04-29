afea8a957e39c7482ddce7e78def7ad121ce90a1
Nainesh <72249854+nainesh-20@users.noreply.github.com>
2024-04-28 21:58:34 -0400

    Update Inventory.js updated the delete function

--- Frontend/src/pages/Inventory.js
+++ Frontend/src/pages/Inventory.js
@@ -69,7 +69,7 @@ function Inventory() {
   const deleteItem = (id) => {
     console.log("Product ID: ", id);
     console.log(`https://meditrack-4w9w.onrender.com/api/product/delete/${id}`);
-    fetch(`http://localhost:4012/api/product/delete/${id}`)
+    fetch(`https://meditrack-4w9w.onrender.com/api/product/delete/${id}`)
       .then((response) => response.json())
       .then((data) => {
         setUpdatePage(!updatePage);
